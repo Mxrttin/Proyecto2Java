0b228869ae3903b044a4b2e081f4cf92c3f5dc74
Martin Irarrazabal <m.irarrazabalj@gmail.com>
2024-06-02 23:11:47 -0400

    comics arreglados

---
+++ assets/js/libros.js
@@ -0,0 +1,142 @@
+'use strict';
+export const comics = [
+    {
+        "serie": "Marvel",
+        "id": "001",
+        "publicaion": "Junio 10, 1992",
+        "name": "Infinity War #1",
+        "escritor": "Starlin Lim",
+        "descripcion":"Cuando aparecen los malvados dobles de los héroes de Marvel, ¡es una guerra total! ¿Por qué Magus los ha desatado en un mundo desprevenido? ¿Y los héroes son la única esperanza... Thanos?",
+        "sprite1":"https://cdn.marvel.com/u/prod/marvel/i/mg/3/40/51f696c2a1263/clean.jpg" ,
+        "precio": "$ 20.000",
+    },
+
+    {
+        "serie": "Marvel",
+        "id": "002",
+        "publicaion": "Mayo 10, 1984",
+        "name": "Secret Wars #1",
+        "escritor": "Jim Shooter",
+        "descripcion":"Mike Zeck ha irritado a algunos de los héroes más grandes y malos de Marvel y en busca de un rasguño. No querrás meterte con este grupo, confía en nosotros. El Beyonder lo aprendió por las malas.",
+        "sprite1": "https://cdn.marvel.com/u/prod/marvel/i/mg/9/b0/51dee6dd91671/clean.jpg",
+        "precio": "$ 20.000",
+
+    },
+    {
+        "serie": "Marvel",
+        "id": "003",
+        "publicaion": "Octubre, 1985 - Febrero, 1986",
+        "name": "Daredevil Born Again",
+        "escritor": "Miller, Mazzucchelli",
+        "descripcion":"Karen Page, la exsecretaria de Nelson & Murdock Abogados y novia de Matt Murdock, dejó atrás los años en los que intentaba labrarse una carrera como actriz en Hollywood. Sus planes no salieron como ella esperaba, se convirtió en una estrella de películas pornográficas y en una yonki heroinómana. Su adicción finalmente la condujo a vender la identidad secreta de Matt por una papelina de heroína.",
+        "sprite1": "https://th.bing.com/th/id/R.f6200f8cf76ea4ccb26c883ec253c9d5?rik=KwzigzfDrof%2b7Q&pid=ImgRaw&r=0",
+        "precio": "$ 20.000",
+
+    },
+    {
+        "serie": "Marvel",
+        "id": "004",
+        "publicaion": "Enero 27, 2016",
+        "name": "Old Man Logan (2016) #1",
+        "escritor": "Lemire, Sorrentino",
+        "descripcion":"Dentro de cincuenta años, Logan, el hombre que ya no se llama a sí mismo Wolverine, habrá soportado muchas atrocidades: los villanos del Universo Marvel se habrán unido y librarán al mundo de sus héroes. El amigo más cercano de Logan, Ojo de Halcón, habrá sido asesinado a sangre fría ante sus ojos. Y enloquecido por la misma radiación que le dio su fuerza sobrehumana, Bruce Banner habrá engendrado una familia de Hulks montañeses... que finalmente mató a la esposa y los dos hijos de Logan.Pero ahora, en el presente, el Viejo Logan se despierta para descubrirse a sí mismo en un mundo anterior a estas atrocidades, antes del Yermo. Y va a aprovechar esta oportunidad y cambiar la historia para asegurarse de que su futuro nunca se cumpla...",
+        "sprite1": "https://cdn.marvel.com/u/prod/marvel/i/mg/a/20/56999ad084bbf/clean.jpg",
+        "precio": "$ 20.000",
+
+    },
+    {
+        "serie": "Marvel",
+        "id": "005",
+        "publicaion": "Julio 05, 2017",
+        "name": "Deadpool Kills the Marvel Universe Again (2017) #1",
+        "escritor": "Bunn, Talajic",
+        "descripcion":"¿Recuerdas la vez que Deadpool se volvió demasiado loco y mató a todo el Universo Marvel? Bueno, esto no es eso. Este es un momento DIFERENTE. El escritor Cullen Bunn y el artista Dalibor Talajic (también conocido como los creadores detrás de Deadpool Kills the Marvel Universe) se han reunido para OTRA historia de Deadpool eliminando a todos tus favoritos de las formas más horribles posibles. Le va a doler más a él que a ti... ¡Y te va a encantar!",
+        "sprite1": "https://cdn.marvel.com/u/prod/marvel/i/mg/e/00/5955392a07557/clean.jpg",
+        "precio": "$ 20.000",
+
+    },
+    {
+        "serie": "Marvel",
+        "id": "006",
+        "publicaion": "Marzo 06, 2013",
+        "name": "Age of Ultron (2013) #1",
+        "escritor": "Bendis, Pacheco",
+        "descripcion":"Durante años, los héroes del Universo Marvel han vivido con el temor de que la inteligencia artificial conocida como Ultrón algún día evolucionara para cumplir su deseo de acabar con toda la vida orgánica y apoderarse de la Tierra: ese día ha llegado. Este evento masivo de 10 partes que abarca el Universo Marvel es presentado por el escritor ganador del premio Eisner Brian Michael Bendis y la leyenda de los cómics Bryan Hitch.",
+        "sprite1": "https://cdn.marvel.com/u/prod/marvel/i/mg/c/d0/51ed8cf71fd82/clean.jpg",
+        "precio": "$ 20.000",
+
+    },
+    
+]
+
+    export const mangas = [
+    {
+        "serie": "Manga",
+        "id": "001",
+        "publicaion": "Noviembre 30, 2013",
+        "name": "Vagabond #1",
+        "escritor": "Takehiko Inoue",
+        "descripcion":"Musashi Miyamoto: el espadachín más famoso de Japón, creador del estilo de las dos espadas (Niten Ichi-ryu) y célebre por permanecer invicto tras 61 duelos a lo largo de sus sesenta años de vida. De la pluma de Takehiko Inoue, el maestro detrás de Slam Dunk, llega este manga cargado de emoción, drama y combates, que narra la historia del legendario guerrero desde sus 17 años, cuando aún era conocido como Takezo Shinmen y recién había participado en la Batalla de Sekigahara. ¡No te pierdas los orígenes de este icónico personaje!",
+        "sprite1":"assets/img/vagabond.jpg" ,
+        "precio": "$ 15.000",
+    },
+
+    {
+        "serie": "Manga",
+        "id": "002",
+        "publicaion": "Octubre 10, 2015",
+        "name": "Vinland Saga #1",
+        "escritor": "Makoto Yukimura",
+        "descripcion":"En el siglo XI, un joven guerrero llamado Thorfinn se une a una banda de mercenarios normandos liderada por Askeladd. Thorfinn solo tiene una cosa en mente: vengar la muerte de su padre, el poderoso guerrero Thors, quien fue asesinado por Askeladd. Para ganarse el derecho a un duelo, nuestro protagonista debe ayudar a su enemigo jurado a cosechar victorias, y así ambos se verán envueltos en una violenta guerra por la corona, todo esto enmarcado por la leyenda de la tierra prometida: la pacífica y eterna Vinland.",
+        "sprite1": "assets/img/vinland.jpg",
+        "precio": "$ 15.000",
+
+    },
+    {
+        "serie": "Manga",
+        "id": "003",
+        "publicaion": "Diciembre 01, 1990",
+        "name": "Berserk #1",
+        "escritor": "Kentaro Miura",
+        "descripcion":"Un hombre completamente vestido de negro que se hace llamar Guts, que tiene una mano artificial de hierro duro y que carga una espada gigantesca que supera su estatura. En el lugar al cual se dirige llueve sangre y hay montañas de cadáveres apilados. Puck, un elfo que se mantiene a su lado desde que Guts le salvó la vida, quedó impresionado por su increíble fuerza, aunque a la vez le causa temor lo violento que es el mundo en el que él vive. Una horda de espíritus malignos que obstaculizan su camino. ¿acaso la marca que tiene en el cuello lo incita a buscar la venganza.?No te pierdas el primer tomo de esta increíble saga.",
+        "sprite1": "assets/img/berserk.jpg",
+        "precio": "$ 15.000",
+
+    },
+    {
+        "serie": "Manga",
+        "id": "004",
+        "publicaion": "Octubre 01, 2013",
+        "name": "Hunter X Hunter #1",
+        "escritor": "Yoshihiro Togashi",
+        "descripcion":"Un mundo fantástico en el que existen bestias maléficas, tesoros ocultos… y hunters o “cazadores”, personas que viven increíbles peripecias para desentrañar los misterios del planeta. Gon no piensa detenerse hasta cumplir su verdadero objetivo: encontrar al legendario hunter Ging Freecss, ¡su padre, que desapareció cuando él era un niño!",
+        "sprite1": "assets/img/hunterx.jpg",
+        "precio": "$ 15.000",
+
+    },
+    {
+        "serie": "Manga",
+        "id": "005",
+        "publicaion": "Noviembre 12, 2004",
+        "name": "One Piece #1",
+        "escritor": "Eiichiro Oda",
+        "descripcion":"Esta es la gran Era Pirata. Gold Roger, el legendario “Rey de los piratas” ha dejado el “One Piece” - su legendario Tesoro - escondido en una zona del mundo llamada “GRAND LINE” y los piratas más fuertes compiten por hallarlo. Un niño llamado Luffy idolatraba a los piratas, en especial a Shanks, el líder de una tripulación que visitó su villa. Un día, por accidente, Luffy comió una “fruta del diablo” del botín de Shanks. Gracias a esa fruta, su cuerpo se ha vuelto de goma pero nunca podrá nadar. El día que los piratas abandonaron la villa, Shanks le obsequió a Luffy su mas preciada posesión: su sombrero de paja. Diez años han pasado desde ese día y Luffy ha salido al mar buscando convertirse en el nuevo “Rey de los piratas”. ¡Aquí inicia la gran aventura de Luffy!",
+        "sprite1": "assets/img/one píece.jpg",
+        "precio": "$ 15.000",
+
+    },
+    {
+        "serie": "Manga",
+        "id": "006",
+        "publicaion": "Marzo 06, 2020",
+        "name": "Jujutsu Kaisen #1",
+        "escritor": "Gege Akutami",
+        "descripcion":"Yuji Itadori es un joven con habilidades físicas excepcionales que se une al club de ocultismo de la escuela porque le permite salir temprano y visitar a su abuelo en el hospital. Este, antes de morir, le pide que use su fuerza para ayudar a las personas. Ahí se aparece un estudiante de hechicero, Fushiguro, y le pide a Yuji el objeto maldito que encontró. Él le dice que sus compañeros del club van a abrirlo en la escuela, y ambos corren a rescatarlos. Al llegar, están rodeados de maldiciones (monstruos), y para salvar a sus compañeros, Yuji se traga el objeto maldito y se convierte en Sukuna.",
+        "sprite1": "assets/img/jujutsu.jpg",
+        "precio": "$ 15.000",
+
+    },
+]
+
+
+
---
+++ assets/js/script.js
@@ -0,0 +1,122 @@
+import {comics} from './libros.js'
+
+const enviarDatos = (serie,id,name,publicaion,escritor,descripcion,sprite1,precio )=>{
+
+    const rutaArchivoHTML = "../libros.html";
+    fetch(rutaArchivoHTML)
+    .then( response => response.text())
+    .then((html)=>{
+
+        const parser = new DOMParser();
+
+        const doc = parser.parseFromString(html,"text/html")
+
+        const imagePage = doc.getElementById("imagePage")
+        imagePage.src = sprite1;
+        imagePage.alt = `Foto con nombre de : ${name}`
+
+        const namePage = doc.getElementById("namePage")
+        namePage.textContent = ` ${name}`
+
+        const escritorPage = doc.getElementById("escritorPage")
+        escritorPage.textContent = `Escritor: ${escritor}`
+
+        const publicacionPage = doc.getElementById("publicaionPage")
+        publicacionPage.textContent = `Publicacion: ${publicaion}`
+
+        const descripcionPage = doc.getElementById("descripcionPage")
+        descripcionPage.textContent=`${descripcion}`
+
+        const precioPage=doc.getElementById("precioPage")
+        precioPage.textContent=`Precio: ${precio}`
+
+
+        const nuevoHTML = new XMLSerializer().serializeToString(doc);
+
+        document.body.innerHTML = nuevoHTML;
+
+    })
+
+
+
+}
+
+const cardContainer = document.getElementById('card-container');
+const {serie,id,name,publicaion,escritor,descripcion,sprite1,precio =""} = comics;
+
+
+const createComicCard = (comics)=>{
+  const {name , escritor , precio , publicaion,descripcion,sprite1 } = comics;
+
+  const card = document.createElement("div");
+  card.classList.add("col", "col-xl-4","col-lg-4","col-md-6", "col-xs-12", "col-sm-12" ,"mt-5" ,"mb-5");
+
+  const cardInner = document.createElement("div");
+  cardInner.classList.add("card");
+
+  const img = document.createElement("img");
+  img.classList.add("card-img-top");
+  img.setAttribute("src", sprite1)
+  img.setAttribute("alt" , name)
+
+  cardInner.appendChild(img)
+
+  const cardBody = document.createElement("div"); 
+  cardBody.classList.add("card-title", "text-center");
+  
+  const title = document.createElement("h3");
+  title.classList.add("card-title" , "mt-3");
+  title.textContent=name;
+
+  const escritorN = document.createElement("p");
+  escritorN.classList.add("card-text");
+  escritorN.textContent = `Escritor : ${escritor}`;
+
+  const publicaionN = document.createElement("p");
+  publicaionN.classList.add("card-text");
+  publicaionN.textContent = `Publicacion : ${publicaion}`;
+
+  const descripcionN = document.createElement("p");
+  descripcionN.classList.add("card-text");
+  descripcionN.textContent = `Escritor : ${descripcion}`;
+
+  const precioInfo = document.createElement("h5");
+  precioInfo.classList.add("card-text" , "mb-4");
+  precioInfo.textContent = `Precio : ${precio}`;
+
+
+  const botonVer = document.createElement("button")
+  botonVer.classList.add("btn" , "btn-success")
+  botonVer.textContent="Ver detalles"
+  botonVer.addEventListener("click" , () =>{
+    enviarDatos(serie,id,name,publicaion,escritor,descripcion,sprite1,precio)
+  })
+
+
+  cardBody.appendChild(title);
+  cardBody.appendChild(escritorN);
+  cardBody.appendChild(precioInfo);
+  cardBody.appendChild(botonVer);
+
+
+
+  cardInner.appendChild(cardBody)
+  card.appendChild(cardInner);
+
+  return card; 
+
+
+
+}
+
+
+const getComics = (comics)=>{
+  const comicCards = comics.map(createComicCard);
+    comicCards.forEach ((card)=>{
+      cardContainer.appendChild(card);
+  });
+}
+
+getComics(comics);
+
+
